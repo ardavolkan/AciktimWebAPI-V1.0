40990a9d303a4e9801133a6c7bd9917ef8bdee52
ardavolkan <125757588+ardavolkan@users.noreply.github.com>
2024-01-31 12:24:19 +0300

    Core eklendi
    Core eklendi

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using Core.CrossCuttingConcerns.Cashing;
+using Core.CrossCuttingConcerns.Cashing.Microsoft;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : IDependencyInjectionModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICasheManager, MemoryCasheManager>();
+            //Birisi senden ICasheManager isterse ona MemoryCasheManager ver.
+            serviceCollection.AddSingleton<Stopwatch>();   // Kronometre
+        }
+    }
+}
---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Core.Entities.Concrete
+{
+   public class UserOperationClaim :IEntity
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string OperationClaimId { get; set; }
+    }
+
+       
+
+}
